26b52c22076d4f4ca72f98757c7e10cef96f9d7e
Vnrx0619 <2848970277@qq.com>
2019-11-21 17:14:30 +0800

    add “销售出库单插件” by 南瑞祥 at 20191121

--- FXBZ_ProdAndMarketOpt/VNRX.FXBZ.SaleOutStockBill.OperationPlugIn/SavePlugIn.cs
+++ FXBZ_ProdAndMarketOpt/VNRX.FXBZ.SaleOutStockBill.OperationPlugIn/SavePlugIn.cs
@@ -33,74 +33,144 @@ namespace VNRX.FXBZ.SaleOutStockBill.OperationPlugIn
                     // 获取当前物料的内码
                     long materialId = Convert.ToInt64(materialObj["Id"]);
 
-                    // 获取当前物料的包装单号 
-                    String packNo = Convert.ToString(col1[i]["F_scfg_PackageCode"]);
 
-                    // 若不为空，则是进行打包的物料
-                    if (!String.IsNullOrWhiteSpace(packNo))
+                    // 获取当前物料行的销售订单单号
+                    String saleBillNo = Convert.ToString(col1[i]["SoorDerno"]);
+                    // 通过该销售订单单号获取生产入库单中该物料的全部的入库重量
+                    StringBuilder tmpSQL0 = new StringBuilder();
+                    tmpSQL0.AppendFormat(@"/*dialect*/ SELECT SUM(E.FREALQTY) INNUM,
+	                                                        SUM(E.F_SCFG_DECIMAL) GENUM,
+                                                            SUM(F_SCFG_DECIMAL2) JIANNUM,
+                                                            SUM(F_SCFG_DECIMAL4) M2NUM,
+                                                            SUM(F_SCFG_DECIMAL6) ZHANGNUM,
+                                                            SUM(F_SCFG_DECIMAL8) XIANGNUM
+                                                            FROM T_PRD_INSTOCKENTRY E 
+                                                            WHERE E.FMOBILLNO IN (SELECT FBILLNO FROM T_PRD_MO WHERE F_SCFG_TEXT = '{0}') AND E.FMATERIALID = {1} ", saleBillNo, materialId);
+                    DynamicObjectCollection col0 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL0.ToString());
+
+
+                    if (col0 != null && col0.Count > 0)
                     {
-                        // 根据包装单号查找对应的条码包装记录，将明细行中的各计量单位数量字段取出，赋值到销售出库单明细行，对应物料的对应数量字段
-                        // 根据包装单号获取上游条码拆装单中该物料的信息
-                        String tmpSQL1 = String.Format(@"/*dialect*/ SELECT * FROM t_UN_PackagingEntry PE LEFT JOIN t_UN_Packaging P ON PE.FID = P.FID WHERE P.FPACKAGING = '{0}' AND PE.FITEMID = {1} ", packNo, materialId);
-                        DynamicObjectCollection col2 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL1);
+                        // 该物料的全部实际入库重量
+                        double realInWeight = Convert.ToDouble(col0[0]["INNUM"]); // 该物料全部已入库的数量
 
-                        if (col2 != null && col2.Count > 0)
-                        {
-                            // 将条码拆装单中的各个计量单位的数值赋值到销售出库单
-                            this.View.Model.SetValue("F_SCFG_M2NUM", col2[0]["F_SCFG_M2NUM"], i);
-                            this.View.Model.SetValue("F_SCFG_ZHANGNUM", col2[0]["F_SCFG_ZHANGNUM"], i);
-                            this.View.Model.SetValue("F_SCFG_GENUM", col2[0]["F_SCFG_GENUM"], i);
-                            this.View.Model.SetValue("F_SCFG_XIANGNUM", col2[0]["F_SCFG_XIANGNUM"], i);
-                            this.View.Model.SetValue("F_SCFG_JIANNUM", col2[0]["F_SCFG_JIANNUM"], i);
-                        }
-                    }
-                    else
-                    {
-                        // 根据物料条码查找条码主档中的数量字段得到公斤数，根据物料单位换算关系计算5个计量单位的数量，并赋值到各个字段上
-                        double realWeight = Convert.ToDouble(col1[i]["FREALQTY"]);
+                        // 查询该物料历史出库总数量
+                        StringBuilder querySQL0 = new StringBuilder();
+                        querySQL0.AppendFormat(@"/*dialect*/ SELECT SUM(FREALQTY) OUTNUM,
+	                                   SUM(F_SCFG_M2NUM) M2NUM,
+	                                   SUM(F_SCFG_ZHANGNUM) ZHANGNUM,
+	                                   SUM(F_SCFG_GENUM) GENUM,
+	                                   SUM(F_SCFG_XIANGNUM) XIANGNUM,
+	                                   SUM(F_SCFG_JIANNUM) JIANNUM
+                                FROM T_SAL_OUTSTOCKENTRY E
+                                LEFT JOIN T_SAL_OUTSTOCKENTRY_R ER
+                                ON E.FID = ER.FID
+                                WHERE ER.FSOORDERNO = '{0}' AND E.FMATERIALID = {1} ", saleBillNo, materialId);
+                        DynamicObjectCollection col00 = DBUtils.ExecuteDynamicObject(this.Context, querySQL0.ToString());
+                        if (col00 != null && col00.Count > 0){
 
-                        StringBuilder tmpSQL2 = new StringBuilder();
-                        tmpSQL2.AppendFormat(@"/*dialect*/ SELECT * FROM T_BD_UNITCONVERTRATE UC LEFT JOIN T_BD_UNIT_L UL ON UL.FUNITID = UC.FCURRENTUNITID WHERE FMATERIALID = '{0}' ", materialId);
-                        DynamicObjectCollection col2 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL2.ToString());
-                        if (col2 != null && col2.Count > 0)
-                        {
-                            // 遍历当前物料的标准称重单位（公斤）与其他称重单位的转换参数
-                            foreach (DynamicObject obj2 in col2)
+                            // 该物料在该订单中已出库数量
+                            double realOutWeight = Convert.ToDouble(col00[0]["OUTNUM"]);
+
+                            // 销售出库单中的物料出库数量
+                            double realWeight = Convert.ToDouble(col1[i]["RealQty"]);
+
+
+                            // 获取当前物料的包装单号 
+                            String packNo = Convert.ToString(col1[i]["F_scfg_PackageCode"]);
+
+                            // 若不为空，则是进行打包的物料
+                            if (!String.IsNullOrWhiteSpace(packNo))
                             {
-                                // 目标称重单位数量
-                                double rate1 = Convert.ToDouble(obj2["FCONVERTDENOMINATOR"]);
-                                // 标准计量称重单位数量
-                                double rate2 = Convert.ToDouble(obj2["FCONVERTNUMERATOR"]);
+                                // 根据包装单号查找对应的条码包装记录，将明细行中的各计量单位数量字段取出，赋值到销售出库单明细行，对应物料的对应数量字段
+                                // 根据包装单号获取上游条码拆装单中该物料的信息
+                                String tmpSQL1 = String.Format(@"/*dialect*/ SELECT * FROM t_UN_PackagingEntry PE LEFT JOIN t_UN_Packaging P ON PE.FID = P.FID WHERE P.FPACKAGING = '{0}' AND PE.FITEMID = {1} ", packNo, materialId);
+                                DynamicObjectCollection col2 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL1);
 
-                                // 计算公斤数量转换为各个称重单位的数值
-                                double realOtherWeight = (realWeight / rate2) * rate1;
-
-                                StringBuilder tmpSQL3 = new StringBuilder();
-                                String where = "";
-                                switch (Convert.ToString(obj2["FNAME"]))
+                                if (col2 != null && col2.Count > 0)
                                 {
-                                    case "平方米":
-                                        where = "F_SCFG_M2NUM";
-                                        break;
-                                    case "张":
-                                        where = "F_SCFG_ZHANGNUM";
-                                        break;
-                                    case "个":
-                                        where = "F_SCFG_GENUM";
-                                        break;
-                                    case "箱":
-                                        where = "F_SCFG_XIANGNUM";
-                                        break;
-                                    case "件":
-                                        where = "F_SCFG_JIANNUM";
-                                        break;
-                                    default:
-                                        break;
+                                    if ((realOutWeight + realWeight) < realInWeight)
+                                    {
+                                        // 将条码拆装单中的各个计量单位的数值赋值到销售出库单
+                                        this.View.Model.SetValue("F_SCFG_M2NUM", col2[0]["F_SCFG_M2NUM"], i);
+                                        this.View.Model.SetValue("F_SCFG_ZHANGNUM", col2[0]["F_SCFG_ZHANGNUM"], i);
+                                        this.View.Model.SetValue("F_SCFG_GENUM", col2[0]["F_SCFG_GENUM"], i);
+                                        this.View.Model.SetValue("F_SCFG_XIANGNUM", col2[0]["F_SCFG_XIANGNUM"], i);
+                                        this.View.Model.SetValue("F_SCFG_JIANNUM", col2[0]["F_SCFG_JIANNUM"], i);
+                                    }
+                                    else
+                                    {
+                                        // 需要平尾差
+                                        this.View.Model.SetValue("F_SCFG_M2NUM", Convert.ToDouble(col0[0]["M2NUM"]) - Convert.ToDouble(col00[0]["M2NUM"]), i);
+                                        this.View.Model.SetValue("F_SCFG_ZHANGNUM", Convert.ToDouble(col0[0]["ZHANGNUM"]) - Convert.ToDouble(col00[0]["ZHANGNUM"]), i);
+                                        this.View.Model.SetValue("F_SCFG_GENUM", Convert.ToDouble(col0[0]["GENUM"]) - Convert.ToDouble(col00[0]["GENUM"]), i);
+                                        this.View.Model.SetValue("F_SCFG_XIANGNUM", Convert.ToDouble(col0[0]["XIANGNUM"]) - Convert.ToDouble(col00[0]["XIANGNUM"]), i);
+                                        this.View.Model.SetValue("F_SCFG_JIANNUM", Convert.ToDouble(col0[0]["JIANNUM"]) - Convert.ToDouble(col00[0]["JIANNUM"]), i);
+                                    }
                                 }
+                            }
+                            else
+                            {
+                                // 根据物料条码查找条码主档中的数量字段得到公斤数，根据物料单位换算关系计算5个计量单位的数量，并赋值到各个字段上
 
-                                if (!String.IsNullOrWhiteSpace(where))
+                                StringBuilder tmpSQL2 = new StringBuilder();
+                                tmpSQL2.AppendFormat(@"/*dialect*/ SELECT * FROM T_BD_UNITCONVERTRATE UC LEFT JOIN T_BD_UNIT_L UL ON UL.FUNITID = UC.FCURRENTUNITID WHERE FMATERIALID = '{0}' ", materialId);
+                                DynamicObjectCollection col2 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL2.ToString());
+                                if (col2 != null && col2.Count > 0)
                                 {
-                                    this.View.Model.SetValue(where, col2[0][where], i);
+                                    // 遍历当前物料的标准称重单位（公斤）与其他称重单位的转换参数
+                                    foreach (DynamicObject obj2 in col2)
+                                    {
+                                        // 目标称重单位数量
+                                        double rate1 = Convert.ToDouble(obj2["FCONVERTDENOMINATOR"]);
+                                        // 标准计量称重单位数量
+                                        double rate2 = Convert.ToDouble(obj2["FCONVERTNUMERATOR"]);
+
+                                        // 计算公斤数量转换为各个称重单位的数值
+                                        double realOtherWeight = (realWeight / rate2) * rate1;
+
+                                        StringBuilder tmpSQL3 = new StringBuilder();
+                                        String where = "";
+                                        String key = "";
+                                        switch (Convert.ToString(obj2["FNAME"]))
+                                        {
+                                            case "平方米":
+                                                where = "F_SCFG_M2NUM";
+                                                key = "M2NUM";
+                                                break;
+                                            case "张":
+                                                where = "F_SCFG_ZHANGNUM";
+                                                key = "ZHANGNUM";
+                                                break;
+                                            case "个":
+                                                where = "F_SCFG_GENUM";
+                                                key = "GENUM";
+                                                break;
+                                            case "箱":
+                                                where = "F_SCFG_XIANGNUM";
+                                                key = "XIANGNUM";
+                                                break;
+                                            case "件":
+                                                where = "F_SCFG_JIANNUM";
+                                                key = "JIANNUM";
+                                                break;
+                                            default:
+                                                break;
+                                        }
+
+                                        if (!String.IsNullOrWhiteSpace(where) && !String.IsNullOrWhiteSpace(key))
+                                        {
+                                            if ((realOutWeight + realWeight) < realInWeight)
+                                            {
+                                                this.View.Model.SetValue(where, col2[0][where], i);
+                                            }
+                                            else
+                                            {
+                                                // 需要进行平尾差操作
+                                                this.View.Model.SetValue(where, Convert.ToDouble(col0[0][key]) - Convert.ToDouble(col00[0][key]), i);
+                                            }
+                                        }
+                                    }
                                 }
                             }
                         }
