c72411105b80b704a1da3c63bb63e492cde3e115
Vnrx0619 <vnrx@foxmail.com>
2020-03-31 10:35:47 +0800

    .

---
+++ FXBZ_ProdAndMarketOpt/GYIN.FXBZ.PRDMO.PlugIn/SetEcValueConvertPlugIn.cs
@@ -0,0 +1,109 @@
+﻿using Kingdee.BOS;
+using Kingdee.BOS.App;
+using Kingdee.BOS.App.Data;
+using Kingdee.BOS.Contracts;
+using Kingdee.BOS.Core;
+using Kingdee.BOS.Core.Bill.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Core.Metadata;
+using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn;
+using Kingdee.BOS.Core.Metadata.ConvertElement.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+
+namespace GYIN.FXBZ.PRDMO.PlugIn
+{
+    [Description("销售订单下推生产订单携带延长米系数并计算延长米")]
+     public class SetEcValueConvertPlugIn : AbstractConvertPlugIn
+    {
+        public override void AfterConvert(AfterConvertEventArgs e)
+        {
+            base.AfterConvert(e);
+            ServiceHelper.GetService<IMetaDataService>();
+            ExtendedDataEntity[] array = e.Result.FindByEntityKey("FBillHead");
+            for (int i = 0; i < array.Length; i++)
+            {
+                DynamicObjectCollection dynamicObjectCollection = array[i].DataEntity["TreeEntity"] as DynamicObjectCollection;
+                for (int p = 0; p < dynamicObjectCollection.Count(); p++)
+                {
+                    string FMaterialId = Convert.ToString(dynamicObjectCollection[p]["MaterialId_Id"]);//物料编码
+                    DynamicObject bomObj = dynamicObjectCollection[p]["BomId"] as DynamicObject;
+                    string FNumber = Convert.ToString(bomObj["Number"]);//BOM版本
+                    if (!String.IsNullOrEmpty(FMaterialId.Trim())&&!String.IsNullOrEmpty(FNumber.Trim()))
+                    {
+                        DynamicObject obj = getdataObj(FMaterialId, FNumber);
+                        if (obj != null)
+                        {
+                            double EC = Convert.ToDouble(obj["F_SCFG_EC"]);//延长米系数
+                            dynamicObjectCollection[p]["F_scfg_EC"] = EC;
+                            double num = Convert.ToDouble(dynamicObjectCollection[0]["Qty"]);//数量
+                            double ycm = EC * num;
+                            dynamicObjectCollection[p]["F_scfg_Qty1"] = ycm;//延长米
+                            array[i]["F_scfg_MaterialId"] = FMaterialId;//物料编码
+                            if (Convert.ToDouble(obj["F_SCFG_DIANYUN"])!=0.00)
+                            {
+                                array[i]["F_scfg_Dianyun"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_DIANYUN"]));//电晕工时
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_YINSHUASHANGBAN"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Yinshuashangban"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_YINSHUASHANGBAN"]));//印刷上版(min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_YINSHUA"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Yinshua"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_YINSHUA"]));//印刷(m/min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_TUBU"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Tubu"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_TUBU"]));//涂布(m/min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_FUHE1"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Fuhe1"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_FUHE1"]));//复合一(m/min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_FUHE2"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Fuhe2"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_FUHE2"]));//复合二(m/min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_FUHE3"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Fuhe3"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_FUHE3"]));//复合三(m/min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_FENQIE"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Fenqie"] = getWorkHourData(ycm, Convert.ToDouble(obj["F_SCFG_FENQIE"]));//分切(m/min)
+                            }
+                            if (Convert.ToDouble(obj["F_SCFG_FENQIEZL"]) != 0.00)
+                            {
+                                array[i]["F_scfg_Fenqiezl"] = getWorkHourFQData(num, Convert.ToDouble(obj["F_SCFG_FENQIEZL"]));//分切重量(kg/h)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        //获得上游销售订单明细物料延长米系数
+        public DynamicObject getdataObj(string FMaterialId, string FNumber)
+        {
+            DynamicObject obj = null;
+            string strSQL = string.Format(@"/*dialect*/SELECT F_SCFG_EC,F_SCFG_DIANYUN,F_SCFG_YINSHUASHANGBAN,F_SCFG_YINSHUA,F_SCFG_TUBU,F_SCFG_FUHE1,F_SCFG_FUHE2,F_SCFG_FUHE3,F_SCFG_FENQIE,F_SCFG_FENQIEZL FROM T_ENG_BOM WHERE FMATERIALID = '{0}' and  FNUMBER = '{1}'", FMaterialId, FNumber);
+            DynamicObjectCollection dataCol = DBUtils.ExecuteDynamicObject(this.Context, strSQL);
+            if (dataCol != null && dataCol.Count > 0)
+            {
+                obj = dataCol[0];
+            }
+            return obj;
+        }
+        //计算工时
+        public double getWorkHourData(double ycm, double whType) {
+            return ycm / whType /60;
+        }
+        //分切重量计算工时
+        public double getWorkHourFQData(double num, double whType)
+        {
+            return num / whType;
+        }
+    }
+}
--- FXBZ_ProdAndMarketOpt/VNRX.FXBZ.SaleOutStockBill.OperationPlugIn/SetValuePlugIn.cs
+++ FXBZ_ProdAndMarketOpt/VNRX.FXBZ.SaleOutStockBill.OperationPlugIn/SetValuePlugIn.cs
@@ -35,62 +35,59 @@ namespace VNRX.FXBZ.SaleOutStockBill.OperationPlugIn
                     // 获取当前物料行的批号
                     String lotNo = Convert.ToString(col1[i]["Lot_Text"]);
                 
-                            // 根据物料条码查找条码主档中的数量字段得到公斤数，根据动态换算关系计算5个计量单位的数量，并赋值到各个字段上
-                            StringBuilder tmpSQL2 = new StringBuilder();
-                            tmpSQL2.AppendFormat(@"/*dialect*/ SELECT * FROM T_scfg_MaterialConvert MC LEFT JOIN T_BD_UNIT_L UL ON UL.FUNITID = MC.FUNITID WHERE MC.FMATERIALNUMBER = '{0}' AND F_SCFG_LOTNO = '{1}' ", materialId, lotNo);//批号  对应简单生产入库的出库
-                            DynamicObjectCollection col2 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL2.ToString());
+                    // 根据物料条码查找条码主档中的数量字段得到公斤数，根据动态换算关系计算5个计量单位的数量，并赋值到各个字段上
+                    StringBuilder tmpSQL2 = new StringBuilder();
+                    tmpSQL2.AppendFormat(@"/*dialect*/ SELECT * FROM T_scfg_MaterialConvert MC LEFT JOIN T_BD_UNIT_L UL ON UL.FUNITID = MC.FUNITID WHERE MC.FMATERIALNUMBER = '{0}' AND F_SCFG_LOTNO = '{1}' ", materialId, lotNo);//批号  对应简单生产入库的出库
+                    DynamicObjectCollection col2 = DBUtils.ExecuteDynamicObject(this.Context, tmpSQL2.ToString());
                     // 销售出库单中的物料出库数量
                     double realWeight = Convert.ToDouble(col1[i]["RealQty"]);
                     if (col2 != null && col2.Count > 0)
+                    {
+                        // 遍历当前物料的标准称重单位（公斤）与其他称重单位的转换参数
+                        foreach (DynamicObject obj2 in col2)
+                        {
+                            // 目标称重单位数量
+                            double rate1 = Convert.ToDouble(obj2["FQTY"]);
+
+                            // 计算公斤数量转换为各个称重单位的数值
+                            double realOtherWeight = Math.Round((double)(realWeight * rate1), 2, MidpointRounding.AwayFromZero);
+                            
+                            StringBuilder tmpSQL3 = new StringBuilder();
+                            String where = "";
+                            String key = "";
+                            switch (Convert.ToString(obj2["FNAME"]))
                             {
-                                // 遍历当前物料的标准称重单位（公斤）与其他称重单位的转换参数
-                                foreach (DynamicObject obj2 in col2)
-                                {
-                                    // 目标称重单位数量
-                                    double rate1 = Convert.ToDouble(obj2["FQTY"]);
-
-                                    // 计算公斤数量转换为各个称重单位的数值
-                                    double realOtherWeight = Math.Round((double)(realWeight * rate1), 2, MidpointRounding.AwayFromZero);
-
-
-                                    StringBuilder tmpSQL3 = new StringBuilder();
-                                    String where = "";
-                                    String key = "";
-                                    switch (Convert.ToString(obj2["FNAME"]))
-                                    {
-                                        case "平方米":
-                                            where = "F_SCFG_M2NUM";
-                                            key = "M2NUM";
-                                            break;
-                                        case "张":
-                                            where = "F_SCFG_ZHANGNUM";
-                                            key = "ZHANGNUM";
-                                            break;
-                                        case "个":
-                                            where = "F_SCFG_GENUM";
-                                            key = "GENUM";
-                                            break;
-                                        case "箱":
-                                        where = "F_SCFG_MULNUM";
+                                case "平方米":
+                                    where = "F_SCFG_M2NUM";
+                                    key = "M2NUM";
+                                    break;
+                                case "张":
+                                    where = "F_SCFG_ZHANGNUM";
+                                    key = "ZHANGNUM";
+                                    break;
+                                case "个":
+                                    where = "F_SCFG_GENUM";
+                                    key = "GENUM";
+                                    break;
+                                case "箱":
+                                    where = "F_SCFG_MULNUM";
                                     key = "MULNUM";
                                     break;
                                 default:
-                                            break;
-                                    }
-                                    if (!String.IsNullOrWhiteSpace(where) && !String.IsNullOrWhiteSpace(key))                                
-                                        this.View.Model.SetValue(where, Convert.ToDouble(realOtherWeight), i);
-                                      if (where.Equals("F_SCFG_MULNUM"))
-                                     {
-                                       this.View.Model.SetValue(where, Convert.ToDouble(Math.Ceiling(realOtherWeight)), i);
-                                      }
-
-                                        }
-                                    }
-                                }
+                                    break;
                             }
-                        
-           
-            
+                            if (!String.IsNullOrWhiteSpace(where) && !String.IsNullOrWhiteSpace(key))
+                            {
+                                this.View.Model.SetValue(where, Convert.ToDouble(realOtherWeight), i);
+                            }           
+                            if (where.Equals("F_SCFG_MULNUM"))
+                            {
+                                this.View.Model.SetValue(where, Convert.ToDouble(Math.Ceiling(realOtherWeight)), i);
+                            }
+                        }
+                    }
+                }
+            }
             base.BeforeSave(e);
         }
     }
